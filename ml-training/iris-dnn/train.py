64b70e8faea670a2c04fc2053721ff5a3827cd4b
Jakobsen <peerjako@38f9d34e66c7.ant.amazon.com>
2019-05-22 09:21:15 +0200

    Changed training steps

--- ml-training/iris-dnn/train.py
+++ ml-training/iris-dnn/train.py
@@ -29,7 +29,7 @@ iris_estimator = TensorFlow(entry_point='ml-training/iris-dnn/iris_dnn_classifie
                             code_location=custom_code_upload_location,
                             train_instance_count=1,
                             train_instance_type='ml.c4.xlarge',
-                            training_steps=1000,
+                            training_steps=900,
                             evaluation_steps=100)
 
 
